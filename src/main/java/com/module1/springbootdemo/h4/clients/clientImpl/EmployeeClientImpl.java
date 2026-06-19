1fa4a27ebb1ecd326d5fa35d15db82a4110041b1
tanishdahiya <tanishdahiya25@gmail.com>
2026-06-19 12:31:43 +0530

    Logging Added

--- src/main/java/com/module1/springbootdemo/h4/clients/clientImpl/EmployeeClientImpl.java
+++ src/main/java/com/module1/springbootdemo/h4/clients/clientImpl/EmployeeClientImpl.java
@@ -6,6 +6,8 @@ import com.module1.springbootdemo.h4.dto.EmployeeDtoRestClient;
 import com.module1.springbootdemo.h4.dto.EmployeeResponseDtoCliennt;
 import com.module1.springbootdemo.h4.dto.EmployeeResponseSingleDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -16,16 +18,28 @@ import java.util.List;
 @Service
 public class EmployeeClientImpl implements EmployeeClient {
 
+    Logger logger = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     private final RestClient restClient;
 
     @Override
     public EmployeeDtoRestClient getEmployee(Long id) {
+        try{
 
-        EmployeeResponseSingleDto employeeResponse = restClient.get()
-                .uri("/employee/{id}", id)
-                .retrieve()
-                .body(EmployeeResponseSingleDto.class);
-        return employeeResponse.getData();
+            logger.info("Fetching employee with ID: {}", id);
+
+            EmployeeResponseSingleDto employeeResponse = restClient.get()
+                    .uri("/employee/{id}", id)
+                    .retrieve()
+                    .body(EmployeeResponseSingleDto.class);
+            logger.info("Employee response: {}", employeeResponse);
+            return employeeResponse.getData();
+
+        }catch (Exception e){
+            logger.error("Error fetching employee with ID: {}. Exception: {}", id, e.getMessage());
+            throw new RuntimeException("Failed to fetch employee with ID: " + id, e);
+        }
+
     }
 
     @Override
